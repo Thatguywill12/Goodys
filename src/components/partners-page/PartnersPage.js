eabb06fab7c1f83c9f01810288ae1bae1fb84caf
zactech178 <fullstacksoftwaredev@gmail.com>
2021-02-08 20:19:16 -0700

    partner page on working

---
+++ src/components/partners-page/PartnersPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PartnerNav from './partners-components/PartnerNav';
+import PartnerSign from './partners-components/PartnerSign';
+
+
+function PartnersPage() {
+    return (
+        <div>
+            <PartnerNav />
+            <PartnerSign />
+        </div>
+    )
+}
+
+export default PartnersPage;
