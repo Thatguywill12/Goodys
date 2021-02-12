59b68f3edbe93af4b412b3a7b17348a4dcd1bb3a
ThatGuyWill <williamjackson982@gmail.com>
2021-02-12 10:51:37 -0800

    Minor merge fix

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,11 +5,7 @@ import PartnersPage from '../partners-page/PartnersPage';
 import UsersHomePage from '../users-page/UsersHomePage';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import UsersGeneralPage from '../users-general-product-page/UsersGeneralPage';
-<<<<<<< HEAD
-import UsersProductPage from '../users-product-page/UsersProductPage';
-=======
 import RetailerPage from '../retailer-page/RetailerPage';
->>>>>>> f33438626e3b8494632afc16764b051de167113d
 
 function App() {
   return (
@@ -21,11 +17,7 @@ function App() {
           <Route exact path="/partners-page" component={PartnersPage} />
           <Route exact path="/users-home-page" component={UsersHomePage} />
           <Route exact path="/users-general-product-page" component={UsersGeneralPage} />
-<<<<<<< HEAD
-          <Route exact path="/users-product-page" component={UsersProductPage} />
-=======
           <Route exact path="/retailer" component={RetailerPage} />
->>>>>>> f33438626e3b8494632afc16764b051de167113d
         </Switch>
       </div>
     </Router>
