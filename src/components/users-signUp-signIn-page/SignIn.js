765291ea5851d36fd4bf57b5469a2323c5f28432
zactech178 <fullstacksoftwaredev@gmail.com>
2021-03-09 09:39:44 -0700

    rotating spin while signing

--- src/components/users-signUp-signIn-page/SignIn.js
+++ src/components/users-signUp-signIn-page/SignIn.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
 import Login from "./Login";
 import ajaxUrl from '../../config/ajaxUrl';
+import CircularProgress from '@material-ui/core/CircularProgress';
+//import { classes } from "istanbul-lib-coverage";
 
+const useStyles = makeStyles((theme) => ({
+    spin: {
+        position: 'fixed',
+        width: '100px !important',
+        height: '100px !important',
+        top: 'calc(50vh - 50px)',
+        left: 'calc(50vw - 50px)',
+    }
+}));
 
 function SignIn({dispatchToStore}) {
+
+    const classes = useStyles();
+
     //Sets use state for User
     const [username, setUsername] = useState("");
     //Sets use state for Email
@@ -20,6 +35,8 @@ function SignIn({dispatchToStore}) {
     //Toggles between SignIn & SignUp
     const [hasAccount, setHasAccount] = useState(false);
 
+    const [signProcess, setSignProcess] = useState(false);
+
     const options = {
         method: 'GET',
         headers: {
@@ -32,6 +49,7 @@ function SignIn({dispatchToStore}) {
     const handleLogin = () => {
         console.log(username, password, email);
         if (username === '' && email === '') return false;
+        setSignProcess(true);
         fetch(`${ajaxUrl}/sign_in?username=${username}&password=${password}&email=${email}`, options).then(res=>res.json().then(data=>{
             console.log(data);
             if(data['sign_in']){
@@ -40,6 +58,7 @@ function SignIn({dispatchToStore}) {
             } else {
                 alert('Username or password is incorrect.');
             }
+            setSignProcess(false);
         }));
     }
 
@@ -53,34 +72,38 @@ function SignIn({dispatchToStore}) {
             alert('confirm password');
             return;
         }
-
+        setSignProcess(true);
         fetch(`${ajaxUrl}/sign_up?username=${username}&password=${password}&email=${email}`, options).then(res=>res.json().then(data=>{
             console.log(data);
             if(data['sign_up']){
                 dispatchToStore('sign_in', {username, password, email});
                 history.push('/');
             }
+            setSignProcess(false);
         }));
-        history.push('/');
     }
     
     return (
         <div>
-            <Login
-                username={username}
-                setUsername={setUsername}
-                email={email}
-                setEmail={setEmail}
-                password={password}
-                setPassword={setPassword}
-                hasAccount={hasAccount}
-                setHasAccount={setHasAccount}
-                emailError={emailError}
-                passwordError={passwordError}
-                handleLogin={handleLogin}
-                handleSignup={handleSignup}
-                setConfirmPassword={setConfirmPassword}
-            />
+            {signProcess?(
+                <CircularProgress className={classes.spin} />
+            ):(
+                <Login
+                    username={username}
+                    setUsername={setUsername}
+                    email={email}
+                    setEmail={setEmail}
+                    password={password}
+                    setPassword={setPassword}
+                    hasAccount={hasAccount}
+                    setHasAccount={setHasAccount}
+                    emailError={emailError}
+                    passwordError={passwordError}
+                    handleLogin={handleLogin}
+                    handleSignup={handleSignup}
+                    setConfirmPassword={setConfirmPassword}
+                />
+            )}
         </div>
     )
 }
